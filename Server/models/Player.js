c99864546a0e54e7f70b68eec3506ccc516630f6
PhatBen234 <158719289+PhatBen234@users.noreply.github.com>
2025-06-07 12:00:35 +0700

    chinh model speed luc dau ve 0 de de~ handle

--- Server/models/Player.js
+++ Server/models/Player.js
@@ -5,7 +5,7 @@ class Player {
     this.score = 0;
     this.alive = true;
     this.position = startPosition;
-    this.direction = { x: 1, y: 0 };
+    this.direction = { x: 0, y: 0 };
     this.speed = 5;
     this.body = [{ ...startPosition }];
     this.length = 1;
